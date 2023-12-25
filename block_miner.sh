7062b1596a45d12a7ca5e1bf8abbcdd20f5474f6
Tannic Archer <113786861+TannicArcher@users.noreply.github.com>
2023-12-25 09:37:26 +0200

    Update block_miner.sh

--- block_miner.sh
+++ block_miner.sh
@@ -13,9 +13,18 @@ fi
 sudo iptables -F
 sudo iptables -X
 
-# Block incoming and outgoing connections to the XMRIG ports
+# Block incoming and outgoing connections to the Miner ports
 for port in "${MINING_PORTS[@]}"; do
     sudo iptables -A INPUT -p tcp --dport "$port" -j DROP
     sudo iptables -A OUTPUT -p tcp --dport "$port" -j DROP
     echo "Miner mining activity blocked on port $port"
+done
+
+# Find and kill processes using the specified ports
+for port in "${MINING_PORTS[@]}"; do
+    pid=$(sudo lsof -t -i tcp:"$port")
+    if [ -n "$pid" ]; then
+        echo "Found process using port $port. Terminating process $pid"
+        sudo kill "$pid"
+    fi
 done
