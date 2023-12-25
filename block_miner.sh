d8f16aa9c6c1dd08bc571228543c44041ac16fd6
Tannic Archer <113786861+TannicArcher@users.noreply.github.com>
2023-12-25 09:34:46 +0200

    Create block_miner.sh

---
+++ block_miner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define the ports used by Miner
+MINING_PORTS=(3333 5555 7777 9000 8080 80 443)
+
+# Check if iptables is installed
+if ! command -v iptables &> /dev/null; then
+    echo "iptables command not found. Please make sure iptables is installed."
+    exit 1
+fi
+
+# Flush existing iptables rules related to Miner
+sudo iptables -F
+sudo iptables -X
+
+# Block incoming and outgoing connections to the XMRIG ports
+for port in "${MINING_PORTS[@]}"; do
+    sudo iptables -A INPUT -p tcp --dport "$port" -j DROP
+    sudo iptables -A OUTPUT -p tcp --dport "$port" -j DROP
+    echo "Miner mining activity blocked on port $port"
+done
